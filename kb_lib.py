4bc1bdcf58669d554d013955d0e5f7712f7083be
Nathan Nguyen <79181764+nathantoannguyen@users.noreply.github.com>
2022-08-24 10:38:49 -0700

    Update kb_lib.py

--- kb_lib.py
+++ kb_lib.py
@@ -1,8 +1,12 @@
+import random
+
+def rollDie() -> int:
+    return random.randint(1,6)
 
 def promptMove() -> int:
     '''
     Prompts current player to enter a valid column number
-    and returns the col num if valid
+    and returns the col num if valid (1-3, int)
     '''
     while True:
         print("Please input which column (1-3) from left to right you would like to place your die in: ")
