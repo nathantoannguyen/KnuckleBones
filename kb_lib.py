fcbeeacff9912100a2cc2542540c82098308612f
Nathan Nguyen <79181764+nathantoannguyen@users.noreply.github.com>
2022-08-25 09:53:12 -0700

    Adjusted UI

--- kb_lib.py
+++ kb_lib.py
@@ -1,21 +1,21 @@
 
+
 import random
 
 def rollDie() -> int:
     dieNum = random.randint(1,6)
-    print(f"You rolled a {dieNum}!")
+    print(f"You rolled a {dieNum}!\n")
     return dieNum
 
 def invalidInput():
     print("Please enter a valid column number of 1, 2, or 3.\n")
 
-def promptMove(kb) -> int:
+def promptMove() -> int:
     '''
     Prompts current player to enter a valid column number
-    and returns the col num if valid (1-3, int)
+    :return: the col number if valid (1-3, int) and random die number
     '''
     while True:
-        current_turn(kb)
         print("Please input which column (1-3) from left to right you would like to place your die in: ")
         col = input()
         if not col.isdigit(): 
@@ -38,7 +38,7 @@ def firstMove(kb):
 
 def current_turn(kb):
     person = "Top" if kb.turn == 0 else "Bottom"
-    print(f"It is {person}'s turn.\n")
+    print(f"It is {person}'s turn.")
 
 
 def show_gb(top, bot):
