7fc6633c9144136e586e3488f8b5a76454a24979
Nathan Nguyen <79181764+nathantoannguyen@users.noreply.github.com>
2022-08-26 11:27:46 -0700

    Updated UI and added winner

--- kb_console.py
+++ kb_console.py
@@ -1,20 +1,23 @@
 import kb
-import kb_lib as lib
+import kb_UI as UI 
 
+# Main game module
 
 def start_game():
-    
+    '''
+    Starts KnuckleBones game
+    '''
     print("\nWelcome to a very scuffed KnuckleBones game on console\n")
 
     game = kb.KnuckleBones()
-    lib.firstMove(game)
+    UI.firstMove(game)
 
     while not game.boardFull():
-        lib.show_gb(game.top, game.bot)
-        lib.current_turn(game)
-        dieNum = lib.rollDie()
+        UI.show_gb(game.top, game.bot)
+        UI.current_turn(game)
+        dieNum = UI.rollDie()
         while True:
-            colNum = lib.promptMove()
+            colNum = UI.promptMove()
             try:
                 game.place_move(game.turn, colNum, dieNum)
                 game.remove_die(game.turn, colNum, dieNum)
@@ -24,20 +27,8 @@ def start_game():
                 lib.fullColMsg()
                 
     game.update_scores()
-    lib.show_gb(game.top, game.bot)
-
-# TODO
-# Add UI for game overs and method for checking winner
-
-# while True
-    # prompt move
-    # check if move is valid
-    # place move
-    # checkIfCancelsOut
-    # checkifBoardFull
-    # if board is full, display final board with score
-# TODO: Show gb again every time they enter an invalid input?
-#       Implement score with showing gb
+    UI.show_gb(game.top, game.bot)
+    UI.winner_output(game.winner())
 
 if __name__ == '__main__':
     start_game()
